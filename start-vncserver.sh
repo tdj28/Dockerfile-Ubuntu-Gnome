d678799298d808175b7d546f28042856f42c2ec1
apkaz <admin@example.com>
2015-04-04 12:14:44 +0200

    updated Dockerfile for supervisord

---
+++ start-vncserver.sh
@@ -0,0 +1,25 @@
+#! /usr/bin/env bash
+# thanks to kolypto @ serverfault for this script.
+# since vncserver is running as a daemon, we're creating a foreground process for supervisord.
+
+set -eu
+
+pidfile="/root/.vnc/b11ec321727f:1.pid"
+command=/usr/bin/vncserver :1 -geometry 1366x768 -depth 24
+
+# Proxy signals
+function kill_app(){
+    kill $(cat $pidfile)
+    exit 0 # exit okay
+}
+trap "kill_app" SIGINT SIGTERM
+
+# Launch daemon
+USER=root $command
+sleep 2
+
+# Loop while the pidfile and the process exist
+while [ -f $pidfile ] && kill -0 $(cat $pidfile) ; do
+    sleep 0.5
+done
+exit 1000 # exit unexpected
