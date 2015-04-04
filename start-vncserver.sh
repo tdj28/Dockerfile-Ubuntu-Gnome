8e450558eab0f37f2b0d47576076466691e68c31
apkaz <admin@example.com>
2015-04-04 22:53:20 +0200

    updated start-vncserver.sh to better handle pid file

--- start-vncserver.sh
+++ start-vncserver.sh
@@ -1,15 +1,17 @@
 #! /usr/bin/env bash
 # thanks to kolypto @ serverfault for this script.
-# since vncserver is running as a daemon, we're creating a foreground process for supervisord.
+# since vncserver is running as a daemon, we're creating a foreground process uppon vncserver for supervisord.
 
 set -eu
 
-# Setting pidfile + command to execute
-pidfile="/root/.vnc/b11ec321727f:1.pid"
-command="/usr/bin/vncserver :1 -geometry 1366x768 -depth 24"
-
 USER=root
 export USER
+
+# Setting pidfile + command to execute
+pidfile="/root/.vnc/*:1.pid"
+command="/usr/bin/vncserver :1 -geometry 1366x768 -depth 24"
+
+
 
 
 # Proxy signals
